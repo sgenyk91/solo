51b68baf27b9ebe8fa546814939c872fbefce14c
Stephan Genyk <stephangenyk@Stephans-MacBook-Air-2.local>
2014-12-02 01:25:39 -0800

    Image description, submit a review, beginning of CSS

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 var app = angular.module("main", []);
 
-app.controller("RestaurantController", function() {
-	this.restaurant = restaurantList;
+app.controller("ImageController", function() {
+	this.image = 0;
+	this.selectImage = function(setImage) {
+		this.image = setImage;
+	};
+	this.isSelected = function(checkImage) {
+		return this.image === checkImage;
+	}
 });
 
-var restaurantList = [{
-		name: "Taqueria Castillo Mason"
+app.controller("MuralController", function() {
+	this.mural = muralList;
+});
+
+app.controller("ReviewController", function() {
+	this.review = {};
+	this.addReview = function(mural) {
+		console.log("mural", mural);
+		
+		mural.reviews.push(this.review);
+		this.review = {};
+	}
+});
+
+var muralList = [{
+		name: "On the Shoulders of a Wizard",
+		artist: "Mark Bode and Os Gemeos",
+		location: "Turk and Taylor",
+		imageUrl: "http://www.sfmuralarts.com/pics/1627.jpg",
+		reviews: [{
+			author: "Dave",
+			rating: 3.5,
+			comment: "Pretty interesting, but overall very weird"
+		}, {
+			author: "Arthur",
+			rating: 5,
+			comment: "I like shrooms so I like this mural!"
+		}]
 	}, {
-		name: "Taqueria Cancun"
-	}];
+		name: "Eye of the Tiger",
+		artist: "Nychos",
+		location: "Geary and Shannon",
+		imageUrl: "http://images.complex.com/complex/image/upload/t_article_image/gwativwwb1odfywe8fag.jpg",
+		reviews: [{
+			author: "Bob",
+			rating: 5,
+			comment: "Amazing mural!"
+		}]
+	}, {
+		name: "I'd Rather Starve",
+		artist: "Unknown Artist",
+		location: "Clarion Alley and Mission",
+		imageUrl: "http://www.sfmuralarts.com/pics/1748.jpg",
+		reviews: [{
+			author: "Melissa",
+			rating: 4.5,
+			comment: "Very deep."
+	}]
+}];
