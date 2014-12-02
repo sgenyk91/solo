f7663a06208cc3a142f24c327423e624ac9e37d4
Stephan Genyk <stephangenyk@Stephans-MacBook-Air-2.local>
2014-12-02 13:53:36 -0800

    More styling, changes submit a review, image description to the right of image, image resizing

--- app.js
+++ app.js
@@ -18,13 +18,23 @@ app.controller("ReviewController", function() {
 	this.review = {};
 	this.addReview = function(mural) {
 		console.log("mural", mural);
-		
+
 		mural.reviews.push(this.review);
 		this.review = {};
 	}
 });
 
 var muralList = [{
+		name: "Eye of the Tiger",
+		artist: "Nychos",
+		location: "Geary and Shannon",
+		imageUrl: "http://images.complex.com/complex/image/upload/t_article_image/gwativwwb1odfywe8fag.jpg",
+		reviews: [{
+			author: "Bob",
+			rating: 5,
+			comment: "Amazing mural!"
+		}]
+	}, {
 		name: "On the Shoulders of a Wizard",
 		artist: "Mark Bode and Os Gemeos",
 		location: "Turk and Taylor",
@@ -39,23 +49,35 @@ var muralList = [{
 			comment: "I like shrooms so I like this mural!"
 		}]
 	}, {
-		name: "Eye of the Tiger",
-		artist: "Nychos",
-		location: "Geary and Shannon",
-		imageUrl: "http://images.complex.com/complex/image/upload/t_article_image/gwativwwb1odfywe8fag.jpg",
-		reviews: [{
-			author: "Bob",
-			rating: 5,
-			comment: "Amazing mural!"
-		}]
-	}, {
 		name: "I'd Rather Starve",
 		artist: "Unknown Artist",
-		location: "Clarion Alley and Mission",
+		location: "Clarion and Mission",
 		imageUrl: "http://www.sfmuralarts.com/pics/1748.jpg",
 		reviews: [{
 			author: "Melissa",
 			rating: 4.5,
 			comment: "Very deep."
+		}]
+	}, {
+		name: "Boogie Birds",
+		artist: "Chor Boogie",
+		location: "Dore St and Howard St",
+		imageUrl: "http://www.sfmuralarts.com/pics/1634.jpg",
+		reviews: [{
 	}]
+	}, {
+		name: "Beauty in Tragedy",
+		artist: "Meggs",
+		location: "866 Geary St",
+		imageUrl: "http://www.sfmuralarts.com/pics/1671.jpg",
+		reviews: [{
+	}]
+	}, {
+		name: "Hummingbirds",
+		artist: "Precita Eyes",
+		location: "21st and Shotwell",
+		imageUrl: "http://www.sfmuralarts.com/pics/1662.jpg",
+		reviews: [{
+	}]
+	
 }];
